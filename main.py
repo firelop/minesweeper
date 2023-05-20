b45043bbe2c35e218a814001869ff5e72cc8d32f
leon <guennec.leon@gmail.com>
2023-05-20 13:40:21 +0200

    fix files management

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, random, time, hud, windows, game
+import pygame, random, time, hud.hud as hud, hud.menu as menu, game
 from consts import *
 
 pygame.init()
@@ -7,9 +7,6 @@ pygame.display.set_caption("Démineur")
 
 clock = pygame.time.Clock()
 
-difficulty_select_list = hud.SelectList(DIFFICULTIES, (CELL_SIZE // 2, TOP_SIZE // 2), (130, TOP_SIZE // 2))
-police = pygame.font.Font("fonts/Orbitron.ttf", 36)
-
 def bomb_generation(x, y):
     bombs_pos = []
     while len(bombs_pos) < BOMB_AMOUNT:
@@ -17,7 +14,7 @@ def bomb_generation(x, y):
         by = random.randint(0, GRID_SIZE - 1)
         if bx > x-1 and bx < x+1 and by > y-1 and by < y+1:
             bombs_pos.append((bx, by))
-            bomb_list[by][bx] = -1
+            # bomb_list[by][bx] = -1
     
     proximity_values(bombs_pos)
     
@@ -28,19 +25,22 @@ def proximity_values(bombs_pos):
             lx = i % 3
             ly = i // 3
             print(bx - lx, by - ly)
-            if bx - lx >= 0 and by - ly >= 0:
-                if(bomb_list[by - ly][bx - lx] != -1):
-                    bomb_list[by - ly][bx - lx] += 1
-
-
+            # if bx - lx >= 0 and by - ly >= 0:
+                # if(bomb_list[by - ly][bx - lx] != -1):
+                #     bomb_list[by - ly][bx - lx] += 1
 
 game = game.Game(window, DIFFICULTIES[DIFFICULTY])
 
+# HUD VARIABLE
+difficulty_select_list = hud.SelectList(window, DIFFICULTIES, (CELL_SIZE // 2, TOP_SIZE // 2), (130, TOP_SIZE // 2))
+chronometer = hud.Chronometer(window, (GRID_SIZE * CELL_SIZE // 2, TOP_SIZE // 2))
+
+police = pygame.font.Font("fonts/Orbitron.ttf", 36)
 playing = True
 first = True
 clock.tick(30)
 while playing:  # Main loop
-    hud.render_hud(window)
+    hud.render_hud(window, difficulty_select_list, chronometer)
 
     # Display difficulties
     if difficulty_select_list.open :
@@ -58,7 +58,7 @@ while playing:  # Main loop
             if y < 0:
                 break
 
-            cell_value = grid[int(y / CELL_SIZE)][int(x / CELL_SIZE)]
+            cell_value = game.grid[int(y / CELL_SIZE)][int(x / CELL_SIZE)]
 
             if event.button == 1:
                 if cell_value == -1:
@@ -66,12 +66,12 @@ while playing:  # Main loop
                         print("Hey yo")
                         bomb_generation(x, y)
                         first = False
-                    grid[int(y / CELL_SIZE)][int(x / CELL_SIZE)] = 1
+                    game.grid[int(y / CELL_SIZE)][int(x / CELL_SIZE)] = 1
             elif event.button == 3:
                 if cell_value == -1:
-                    grid[int(y / CELL_SIZE)][int(x / CELL_SIZE)] = 2
+                    game.grid[int(y / CELL_SIZE)][int(x / CELL_SIZE)] = 2
                 else:
-                    grid[int(y / CELL_SIZE)][int(x / CELL_SIZE)] = -1
+                    game.grid[int(y / CELL_SIZE)][int(x / CELL_SIZE)] = -1
 
     pygame.display.flip()
 
