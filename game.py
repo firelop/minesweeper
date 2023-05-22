0eee45f176bae3aff284059bac7cd7b8136ff7fa
firelop <49453048+firelop@users.noreply.github.com>
2023-05-22 17:04:24 +0200

    modified:   __pycache__/game.cpython-39.pyc 	modified:   game.py

--- game.py
+++ game.py
@@ -4,33 +4,43 @@ from consts import bomb_amount, grid_size, cell_size, TOP_SIZE, BLUE, GREEN, GRA
 def get_grid(default):
     return [[default for x in range(grid_size.get())] for y in range(grid_size.get())]
 
-def proximity_values(bombs_pos):
+def proximity_values(bomb_list, bombs_pos):
     pass
     # for bx, by in bombs_pos:
-    #     for i in range(9):
-    #         lx = i % 3
-    #         ly = i // 3
-    #         print(bx - lx, by - ly)
-    #         # if b
-    #         # x - lx >= 0 and by - ly >= 0:
-    #             # if(bomb_list[by - ly][bx - lx] != -1):
-    #             #     bomb_list[by - ly][bx - lx] += 1
+    #     for y in range(3):
+    #         for x in range(3):
+    #             tx = (bx - x) + 1
+    #             ty = (by - y) + 1
+
+    #             if ty < 0 or tx < 0:
+    #                 continue
+
+    #             if ty > grid_size.get()-1 or tx > grid_size.get()-1:
+    #                 continue
+
+    #             if bomb_list[ty][tx] == -1:
+    #                 continue
+
+                
+    #             bomb_list[ty][tx] += 1
+
+    # return bomb_list
+    
 
 def bomb_generation(bomb_list, x, y):
     bombs_pos = []
-    print(bomb_list)
-    # while len(bomb_list) < bomb_amount.get():
-        
-    #     pass
-    # bombs_pos = []
-    # while len(bombs_pos) < bomb_amount.get():
-    #     bx = random.randint(0, grid_size.get() - 1)
-    #     by = random.randint(0, grid_size.get() - 1)
-    #     if bx > x-1 and bx < x+1 and by > y-1 and by < y+1:
-    #         bombs_pos.append((bx, by))
-    #         # bomb_list[by][bx] = -1
+    bomb_count = 0
+    bomb_amount.set(grid_size.get()**2 // 8)
+    print(bomb_amount.get())
+    while bomb_count < bomb_amount.get():
+        bx = random.randint(0, grid_size.get() - 1)
+        by = random.randint(0, grid_size.get() - 1)
+        if bx > x+1 or bx < x-1 and by > y+1 or by < y-1:
+            bombs_pos.append((bx, by))
+            bomb_list[by][bx] = -1
+            bomb_count += 1
     
-    # proximity_values(bombs_pos)
+    return proximity_values(bomb_list, bombs_pos)
 
 class Game:
     def __init__(self, window, difficulty):
@@ -60,6 +70,10 @@ class Game:
                 cell_value = self.grid[y][x]
                 if cell_value == 1:
                     self.window.fill(GRAY, (x * cell_size.get(), y * cell_size.get() + TOP_SIZE, cell_size.get(), cell_size.get() + TOP_SIZE))
+                    pol = pygame.font.Font("fonts/Orbitron.ttf", 46)
+                    if(self.bomb_list[y][x] != 0):
+                        txt = pol.render(str(self.bomb_list[y][x]), True, (0, 0, 0))
+                        self.window.blit(pygame.transform.scale(txt, (cell_size.get(), cell_size.get())), (x * cell_size.get(), y * cell_size.get() + TOP_SIZE, cell_size.get(), cell_size.get() + TOP_SIZE))
 
                 elif cell_value == 2:
                     self.window.fill(BLUE if (x + y) % 2 == 0 else GREEN,
@@ -76,13 +90,34 @@ class Game:
                     self.window.fill(BLUE if (x + y) % 2 == 0 else GREEN,
                                 (x * cell_size.get(), y * cell_size.get() + TOP_SIZE, cell_size.get(), cell_size.get() + TOP_SIZE))
 
-                pol = pygame.font.Font("fonts/Orbitron.ttf", 46)
-                txt = pol.render(str(self.bomb_list[y][x]), True, (0, 0, 0))
-                self.window.blit(pygame.transform.scale(txt, (cell_size.get(), cell_size.get())), (x * cell_size.get(), y * cell_size.get() + TOP_SIZE, cell_size.get(), cell_size.get() + TOP_SIZE))
+                
 
             y += 1
 
             self.animation_frame = (self.animation_frame + 0.016) % 4
+
+    def propagate(self, x, y):
+        if(self.bomb_list[y][x] != 0):
+            return
+        for i in range(3):
+            for n in range(3):
+                a = (x - n)+1
+                b = (y - i)+1
+                if a < 0 or b < 0:
+                    continue
+
+                if a > grid_size.get()-1 or b > grid_size.get()-1:
+                    continue
+
+                if self.bomb_list[b][a] == 0:
+                    if self.grid[b][a] == -1:
+                        self.grid[b][a] = 1
+                        self.propagate(a, b)
+                elif self.bomb_list[b][a] != -1:
+                    self.grid[b][a] = 1
+                    
+                
+
 
     def click(self, x, y, event):
         y -= TOP_SIZE
@@ -99,8 +134,9 @@ class Game:
             if cell_value == -1:
                 if(self.first):
                     self.first = False
-                    bomb_generation(self.bomb_list, x//cell_size.get(), y//cell_size.get())
+                    self.bomb_list = bomb_generation(self.bomb_list, x//cell_size.get(), y//cell_size.get())
                 self.grid[y // cell_size.get()][x // cell_size.get()] = 1
+                self.propagate(x//cell_size.get(), y//cell_size.get())
         elif event.button == 3:
             if cell_value == -1:
                 self.grid[y // cell_size.get()][x // cell_size.get()] = 2
